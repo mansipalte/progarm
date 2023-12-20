73d23c0ee4c3dd6f66a17390bb0e20a29836a19d
mansipalte <154410490+mansipalte@users.noreply.github.com>
2023-12-20 13:20:40 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,6 +1,6 @@
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+num1 = input('Enter first number: ')    
+num2 = input('Enter second number: ')    
+ 
+sum = float(num1) + float(num2)    
 
-sum = int(num1) + int(num2)
-
-print('The sum of {0} and {1} is {2}'.int(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))      
